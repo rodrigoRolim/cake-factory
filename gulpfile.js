956b1e11ea716813e99fe5a628306b4d06acaf0e
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-11 17:35:46 -0300

    removing cleaner task and adding server task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch, series } = require('gulp')
+const { src, dest, watch, parallel } = require('gulp')
 const babel = require('gulp-babel')
 const plumber = require('gulp-plumber')
 const clean = require('gulp-clean')
@@ -9,17 +9,9 @@ function isJavascript (file) {
   return file.extname === '.js'
 }
 
-function cleaner (cb) {
-  
-  src('dist', { allowEmpty: true })
-    .pipe(plumber())
-    .pipe(clean())
-  cb()
-}
 
-function builder (cb) {
-
-  src(['src/**/*.js', 'src/config/*.json', 'server.js'])
+function server (cb) {
+  src('server.js')
     .pipe(plumber())
     .pipe(gulpif(isJavascript, babel({
       presets: ['@babel/preset-env']
@@ -27,8 +19,18 @@ function builder (cb) {
     .pipe(dest('build'))
   cb()
 }
+function builder (cb) {
+
+  src(['src/**/*.js', 'src/config/*.json'])
+    .pipe(plumber())
+    .pipe(gulpif(isJavascript, babel({
+      presets: ['@babel/preset-env']
+    })))
+    .pipe(dest('build/src'))
+  cb()
+}
 
 exports.build = builder
 exports.default = function () {
-  watch(['src/**/*.js', 'src/config/*.json', 'server.js'], { ignoreInitial: false }, series(cleaner, builder))
+  watch(['src/**/*.js', 'src/config/*.json', 'server.js'], { ignoreInitial: false }, parallel(builder, server))
 }
