705f11e22a58c156117767e6983c7b22b474a755
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-11 16:23:42 -0300

    creating a clean task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,9 @@ const babel = require('gulp-babel')
 const plumber = require('gulp-plumber')
 const clean = require('gulp-clean')
 
+task ('clean', function () {
+  return src('dist', { allowEmpty: true })
+    .pipe(plumber())
+    .pipe(clean())
+})
+
