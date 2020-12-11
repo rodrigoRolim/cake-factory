7ad477dd99c7a56d46b8d9a8c14a6f46d8a88ac7
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-11 16:38:18 -0300

    creating a transpiler task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
-const { src, dest, watch, series, tasks } = require('gulp')
+const { src, dest, watch, series, tasks, task } = require('gulp')
 const babel = require('gulp-babel')
 const plumber = require('gulp-plumber')
 const clean = require('gulp-clean')
 
-task ('clean', function () {
+function isJavascript (file) {
+
+  return file.extname === '.js'
+}
+
+function clean () {
+  
   return src('dist', { allowEmpty: true })
     .pipe(plumber())
     .pipe(clean())
-})
+}
 
+function transpiler () {
+
+  return src(['src/**/*.js', 'src/config/*.json', 'server.js'])
+    .pipe(plumber())
+    .pipe(isJavascript, babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(dest('build'))
+}
