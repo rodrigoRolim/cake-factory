987a0aa28ee0cb6062aaf0ac4f687604240bdefc
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-12 15:25:13 -0300

    importing routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import database from '../config/database'
+import routes from './routes'
 
 const configureExpress = () => {
 
   const app = express()
   app.use(express.json())
   app.use(express.urlencoded())
-
+  app.use('/', routes)
   return app
 }
 
