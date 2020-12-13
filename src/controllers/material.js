a70693156c7f73011f4560769922a81f32f32f6e
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-13 13:30:18 -0300

    creating a private function to searchin necessary materials

--- src/controllers/material.js
+++ src/controllers/material.js
@@ -3,12 +3,18 @@ class MaterialController {
   constructor (Material) {
     this.Material = Material
   }
+  findNecessaryMaterial (req, res, name) {
+    let reg  = new RegExp(name)
+    return this.Material.find({ name: { $regex: reg } }, { name: 1, quantity: 1, user: 1, _id: 0 })
+            .then((resp) => res.send(resp))
+            .catch(err => res.status(404).send(err.message))
+  }
   search (req, res) {
     let name = req.query.name;
-    let reg  = new RegExp(name)
-    this.Material.find({ name: { $regex: reg } }, { name: 1, quantity: 1, user: 1, _id: 0 })
-      .then((resp) => res.send(resp))
-      .catch(err => res.status(404).send(err.message))
+    let user = req.query.user;
+    if (name) {
+      return this.findNecessaryMaterial(req, res, name)
+    }
   }
   create (req, res) {
     const material = new this.Material(req.body)
