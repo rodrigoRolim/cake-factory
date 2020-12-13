e92426f94d0844a8eb4f6d45b1d8611934da423f
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-13 12:03:23 -0300

    implementing auto_increment integer _id

--- src/models/material.js
+++ src/models/material.js
@@ -1,12 +1,22 @@
 import mongoose from 'mongoose'
 
+const counter = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true
+  },
+  seq: {
+    type: Number,
+    default: 0
+  }
+})
 const writeOff = new mongoose.Schema({
   quantity: Number,
   user: String
 })
 
 const material = new mongoose.Schema({
-  id: Number,
+  _id: Number,
   name: String,
   quantity: Number,
   user: String,
@@ -14,6 +24,6 @@ const material = new mongoose.Schema({
   createdDate: { type: Date, default: Date.now }
 })
 
-const Material = mongoose.model('Material', schema)
+const Material = mongoose.model('Material', material)
 
 export default Material
