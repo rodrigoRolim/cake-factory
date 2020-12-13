3eab7e48b7be1b3e607538c7c5e9a5c0fa75e05a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-13 10:40:45 -0300

    creating controller to Material

---
+++ src/controllers/Material.js
@@ -0,0 +1,20 @@
+class MaterialController {
+  
+  constructor (Material) {
+    this.Material = Material
+  }
+  searchMaterial (req, res) {
+    let name = req.query.name;
+  }
+  createMaterial (req, res) {
+
+  }
+  useMaterial (req, res) {
+
+  }
+  quantitySpendingByBaker (req, res) {
+
+  }
+}
+
+export default MaterialController
