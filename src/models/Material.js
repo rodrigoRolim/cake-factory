0d8c7d28c59bbbd4afc96a1f3b71845ef1b99d7e
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-13 10:43:04 -0300

    create child document: writeOff

--- src/models/Material.js
+++ src/models/Material.js
@@ -2,14 +2,14 @@ import mongoose from 'mongoose'
 
 const writeOff = new mongoose.Schema({
   quantity: Number,
-  
+  user: String
 }) 
 const material = new mongoose.Schema({
   id: Number,
   name: String,
   quantity: Number,
   user: String,
-  writeOffs: [],
+  writeOffs: [writeOff],
   createdDate: { type: Date, default: Date.now }
 })
 
