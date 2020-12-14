cd28d05ec78aadd83635cdaaa1f29394e121b6d5
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-14 08:07:48 -0300

    using a database

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 mongoose.Promise = Promise
 const mongodURL = process.env.MONGODB_URL || 'mongodb://localhost/cake-factory'
-const connect = () => mongoose.connect(mongodURL, { userNewUrlParser: true })
+const connect = () => mongoose.connect(mongodURL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 export default {
   connect
