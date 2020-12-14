fb6533e85286698904db84946813a450251fdfcb
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-14 08:09:31 -0300

    call model and controller of order

--- src/routes/material.js
+++ src/routes/material.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import MaterialController from '../controllers/material'
 import Material from '../models/material'
+import Order from '../models/order'
+import OrderController from '../controllers/order'
 
 const router = express.Router()
-const materialController = new MaterialController(Material)
+const orderController = new OrderController(Order)
+const materialController = new MaterialController(Material, orderController)
 
 router.post('/', (req, res) => materialController.create(req, res))
 router.get('/', (req, res) => materialController.search(req, res))
-router.put('/:id/request', (req, res) => materialController.writeOff(req, res))
+router.put('/:id/request', (req, res) => materialController.check(req, res))
 
 export default router
