f810beb6317128cec9dc1e6e2bb3b29945a2759a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-14 08:08:46 -0300

    creating add order function

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -3,7 +3,53 @@ class OrderController {
   constructor (Order) {
     this.Order = Order
   }
-  check (req, res) {
-    
+  addOrder (req, res) {
+    return new Promise((resolve, reject) => {
+      let objOrder = {
+        quantity: req.body.quantity,
+        user: req.body.user,
+        material: req.params.id
+      }
+      let order = new this.Order(objOrder)
+      order.save()
+        .then(() => resolve(true))
+        .catch(() => reject(false))
+    })
+   
+  }
+ 
+  getOrderByUser (user) {
+    let reg = new RegExp(user)
+    return this.Order.aggregate([
+      {
+        $lookup: {
+          localField: "material",
+          from: "materials",
+          foreignField: "_id",
+          as: "materials"  
+        }
+      },
+      {
+        $match: {
+          "user": { $regex: reg }
+        }
+      },
+      {
+        $unwind: "$materials"
+      },
+      {
+        $addFields: {
+          name: "$materials.name" 
+        }
+      },
+      {
+        $project: {
+          "material": 0,
+          "materials": 0
+        }
+      }
+    ])
   }
 }
+
+export default OrderController
