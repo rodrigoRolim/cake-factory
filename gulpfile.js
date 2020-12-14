053fccb9ac9a6a6370e801c33d69883cc2f2953f
rodrigoRolim <rodrigorolimveras92@gmail.com>
2020-12-14 09:07:55 -0300

    refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,36 @@
-const { src, dest, watch, parallel, series } = require('gulp')
+const { src, dest, watch, series } = require('gulp')
 const babel = require('gulp-babel')
 const plumber = require('gulp-plumber')
-const clean = require('gulp-clean')
-const gulpif = require('gulp-if')
 const nodemon = require('gulp-nodemon')
-function isJavascript (file) {
-
-  return file.extname === '.js'
-}
-
 
 function compileServer (cb) {
   
   src('server.js')
     .pipe(plumber())
-    .pipe(gulpif(isJavascript, babel({
+    .pipe(babel({
       presets: ['@babel/preset-env']
-    })))
+    }))
     .pipe(dest('build'))
   cb()
 }
 
 const compiler = series(compileServer, function (cb) {
 
-  src(['src/**/*.js', 'src/config/*.json'])
+  src(['src/**/*.js'])
     .pipe(plumber())
-    .pipe(gulpif(isJavascript, babel({
-      presets: ['@babel/preset-env']
-    })))
+    .pipe(babel({
+      presets: ['@babel/preset-env'],
+      plugins: [
+        ['@babel/transform-runtime']
+      ]
+    }))
     .pipe(dest('build/src'))
   cb()
 
 })
 function watchCompiler (cb) {
   watch(
-    ['src/**/*.js', 'src/config/*.json', 'server.js'], 
+    ['src/**/*.js'], 
     { ignoreInitial: false }, compiler)
   cb()
 }
@@ -47,17 +43,17 @@ function execute (done) {
       'var/',
       'node_modules/'
     ],
-    watch: ['build/src/**/*.js', 'build/src/config/*.json', 'build/server.js'],
+    watch: ['build/**/**/*.js'],
     done: done
   })
   stream
-  .on('restart', function () {
-    console.log("the server was restarted!")
-  })
-  .on('crash', function () {
-    console.log('The application has crashed\n')
-    stream.emit('restart', 10)
-  })
+    .on('restart', function () {
+      console.log("the server was restarted!")
+    })
+    .on('crash', function () {
+      console.log('The application has crashed\n')
+      stream.emit('restart', 10)
+    })
   done()
 }
 exports.build = compiler
